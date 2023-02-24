4521ef6601c28c1b26218b2ad329a11d4f54a998
subtext916 <russ@russandsonya.com>
2023-02-24 06:22:29 +0100

    update react demo with notification subscription in worker

--- demos/react/src/App.js
+++ demos/react/src/App.js
@@ -5,15 +5,33 @@ function App() {
   const postal = usePostalWorker();
 
   const [test, setTest] = useState(null);
+  const [serializedStore, setSerializedStore] = useState("{}");
+  const [randomNumber, setRandomNumber] = useState(0);
 
   useEffect(() => {
-    postal.postBox("test", update => setTest(update.value));
+    console.info(postal);
+    postal.postBox("test", update => {
+      setTest(update.value);
+      setSerializedStore(JSON.stringify(update.box));
+    });
     postal.post("test", "this is a test");
-  }, [setTest]);
+  }, [setTest, setSerializedStore, postal]);
+
+  useEffect(() => {
+    postal.load("./workerSubscribeDemo.js");
+    postal.on("demoNotification", msg => {
+      setRandomNumber(msg);
+    });
+  }, [setRandomNumber, postal]);
 
   return (
     <div id="App">
-      <div id="demo">{test}</div>
+      <h1>React Demo</h1>
+      <div id="demo">
+        {test} <br />
+        store: {serializedStore}
+      </div>
+      <div id="randomNumber">Random number: {randomNumber}</div>
     </div>
   );
 }
